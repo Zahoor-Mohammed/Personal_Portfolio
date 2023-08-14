dccf0a6d87505d5df0ba0cf8ebdf2a2ffac992ac
Zahoor Mohammed <142212418+Zahoor-Mohammed@users.noreply.github.com>
2023-08-14 14:18:27 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,13 @@
+console.log("Hello world")
+alert("Hello World");
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+}
